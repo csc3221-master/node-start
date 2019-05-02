afef06fbdb5a8d99a613b57872a7bc0460e73081
Michael Bryant <shadow53@shadow53.com>
2019-05-01 23:02:05 -0700

    Extra credit: Add new friends JSON and server
    New JSON file has a different structure. Technically the original code
    this assignment was based on could handle the JSON fine, but these
    changes add the keys as titles and makes each array a separate table.

---
+++ app2.js
@@ -0,0 +1,70 @@
+const http = require('http');
+const fs = require('fs');
+var config = {};
+if (fs.existsSync('./config.json')) config = require('./config.json');
+const hostname = config.hostname || 'localhost';
+const port = config.port || 3005;
+
+var friends = require("./friends2.json"); // Once for all times
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-type', 'text/html');
+	var currentDate = new Date();
+    response.write(
+        `<!DOCTYPE html>
+        <html lang="en">
+                <head>
+                       <meta charset="utf-8">
+                       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                       <meta name="viewport" content="width=device-width, initial-scale=1">
+                       <meta name="description" content="Home Page">
+                       <meta name="author" content="Carlos Arias">
+                       <title>Netcentric Computing Home Page</title>
+                       <!-- Bootstrap core CSS -->
+						<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+               </head>
+                <body>
+                       <div class="container" style="text-align: center">
+                       <h1>Another Example of Node</h1><br>
+                       <p>Current time is: ${currentDate}</p>`
+    );
+	for (var key in friends) {
+            response.write(`
+                       <h2>${key.toUpperCase()}</h2>
+                       <table class="table table-bordered table-hover">
+                               <thead>
+                                       <tr>
+                                               <th scope="col">First Name</th>
+                                               <th scope="col">Last Name</th>
+                                               <th scope="col">Phone</th>
+                                       </tr>
+                               </thead>
+                               <tbody>`
+            );
+	        for (var f in friends[key]) {
+	            response.write(
+	                `                               <tr>
+	                                                       <td>${friends[key][f]["firstName"]}</td>
+	                                                       <td>${friends[key][f]["lastName"]}
+	                                                       <td>${friends[key][f]["phone"]}
+	                                               </tr>
+`
+	            );
+            }
+            response.write(
+            `                       </tbody>
+	                       </table>`
+            );
+    }
+
+	    response.write(
+	        `       </body>
+	        </html>`
+	    );
+	    response.end();
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
