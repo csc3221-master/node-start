07abafff7699a5cf25c297086a22f88e170e7b58
Michael Bryant <shadow53@shadow53.com>
2019-05-01 21:35:49 -0700

    Replace concatenated strings with template strings
    JavaScript has template strings, which allow embedded newlines and
    variable interpolation; this means that much less string trickery needs
    to happen to build the HTML. I've also used this to minimize the number
    of calls to `response.write()`.

--- app.js
+++ app.js
@@ -7,59 +7,53 @@ var friends = require("./friends.json"); // Once for all times
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-type', 'text/html');
-    response.write(
-        '<!DOCTYPE html> \n' +
-        '<html lang="en"> \n' +
-        '        <head> \n' +
-        '               <meta charset="utf-8"> \n' +
-        '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
-        '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
-        '               <meta name="description" content="Home Page"> \n' +
-        '               <meta name="author" content="Carlos Arias"> \n' +
-        '               <title>Netcentric Computing Home Page</title> \n' +
-        '               <!-- Bootstrap core CSS --> \n' +
-		'				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">\n' +
-        '       </head> \n' +
-        '        <body> \n' +
-        '               <div class="container" style="text-align: center"> \n' +
-        '               <h1>Another Example of Node</h1><br> \n'
-
-    );
 	var currentDate = new Date();
     response.write(
-        '               <p>Current time is: ' + currentDate + '</p>'
-    );
-    response.write(
-        '               <table class="table table-bordered table-hover"> \n' +
-        '                       <thead> \n' +
-        '                               <tr> \n' +
-        '                                       <th scope="col">First Name</th> \n' +
-        '                                       <th scope="col">Last Name</th> \n' +
-        '                                       <th scope="col">Phone</th> \n' +
-        '                               </tr> \n' +
-        '                       </thead> \n' +
-        '                       <tbody> \n'
+        `<!DOCTYPE html>
+        <html lang="en">
+                <head>
+                       <meta charset="utf-8">
+                       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                       <meta name="viewport" content="width=device-width, initial-scale=1">
+                       <meta name="description" content="Home Page">
+                       <meta name="author" content="Carlos Arias">
+                       <title>Netcentric Computing Home Page</title>
+                       <!-- Bootstrap core CSS -->
+						<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+               </head>
+                <body>
+                       <div class="container" style="text-align: center">
+                       <h1>Another Example of Node</h1><br>
+                       <p>Current time is: ${currentDate}</p>
+                       <table class="table table-bordered table-hover">
+                               <thead>
+                                       <tr>
+                                               <th scope="col">First Name</th>
+                                               <th scope="col">Last Name</th>
+                                               <th scope="col">Phone</th>
+                                       </tr>
+                               </thead>
+                               <tbody>
+`
     );
 	for (var key in friends)
 	        for (var f in friends[key])
 	            response.write(
-	                '                               <tr> \n' +
-	                '                                       <td>' + friends[key][f]["firstName"] + '</td> \n' +
-	                '                                       <td>' + friends[key][f]["lastName"] + '</td> \n' +
-	                '                                       <td>' + friends[key][f]["phone"] + '</td> \n' +
-	                '                               </tr> \n'
+	                `                               <tr>
+	                                                       <td>${friends[key][f]["firstName"]}</td>
+	                                                       <td>${friends[key][f]["lastName"]}
+	                                                       <td>${friends[key][f]["phone"]}
+	                                               </tr>
+`
 	            );
 
 	    response.write(
-	        '                       </tbody> \n' +
-	        '               </table> \n'
-	    );
-	    response.write(
-	        '       </body> \n' +
-	        '</html> \n'
+	        `                       </tbody>
+	                       </table>
+	               </body>
+	        </html>`
 	    );
 	    response.end();
-
 });
 
 server.listen(port, hostname, () => {
